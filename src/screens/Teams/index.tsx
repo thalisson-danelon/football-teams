fe984fa153ac35764e9e2a50734e001d8afeb7b1
thalisson-danelon <thalisson.github@gmail.com>
2024-08-17 14:51:04 -0300

    feat: ListEmpty component created

--- src/screens/Teams/index.tsx
+++ src/screens/Teams/index.tsx
@@ -1,12 +1,14 @@
 import { Container } from '@screens/Teams/styles';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
-import { GroupCard } from '@components/GroupCard';
+import { TeamCard } from 'src/components/TeamCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Teams() {
-  const [teams, setTeams] = useState<string[]>(['Os Pernetas', 'Pernas de Pau', 'Tiririca', 'Cruzeiro do Sul']);
+  // const [teams, setTeams] = useState<string[]>(['Os Pernetas', 'Pernas de Pau', 'Tiririca', 'Cruzeiro do Sul']);
+  const [teams, setTeams] = useState<string[]>([]);
 
   return (
     <Container>
@@ -15,7 +17,9 @@ export function Teams() {
       <FlatList
         data={teams}
         keyExtractor={item => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={teams.length === 0 && { flex: 1 }}
+        renderItem={({ item }) => <TeamCard title={item} />}
+        ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar o primeiro time?" />}
       />
     </Container>
   );
