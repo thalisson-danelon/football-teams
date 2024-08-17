0e3c2757ceb92a5b838d46639b8e507644f2b791
thalisson-danelon <thalisson.github@gmail.com>
2024-08-17 00:52:30 -0300

    feat: HighLigth component created

--- src/screens/Goups/index.tsx
+++ src/screens/Goups/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from '@screens/Goups/styles';
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title="Times" subtitle="Jogue com o seu time" />
     </Container>
   );
 }
