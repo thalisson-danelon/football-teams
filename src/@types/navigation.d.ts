3e5b33d24a0e8a082656e52ae961d2ded721685e
thalisson-danelon <thalisson.github@gmail.com>
2024-08-18 21:50:47 -0300

    feat: navigation type created

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      teams: undefined;
+      newTeam: undefined;
+      players: {
+        team: string
+      };
+    }
+  }
+}
