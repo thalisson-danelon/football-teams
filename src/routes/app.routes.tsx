79438d5efb67f732c7ff2b29906be527be6e3459
thalisson-danelon <thalisson.github@gmail.com>
2024-08-18 21:06:46 -0300

    feat: app routes created

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Teams } from '@screens/Teams';
+import { Players } from '@screens/Players';
+import { NewTeam } from '@screens/NewTeam';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes() {
+  return (
+    <Navigator>
+      <Screen name="teams" component={Teams} />
+      <Screen name="players" component={Players} />
+      <Screen name="newTeam" component={NewTeam} />
+    </Navigator>
+  );
+}
