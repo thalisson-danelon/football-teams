cddce3758173af51aa936dbf39b9c42579c04e7e
thalisson-danelon <thalisson.github@gmail.com>
2024-08-18 21:04:36 -0300

    feat: Players screen updated

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
-import { Container, Form } from './styles';
+import { Filter } from '@components/Filter';
+import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
+import { Button } from '@components/Button';
 
 export function Players() {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -13,6 +22,41 @@ export function Players() {
         <Input placeholder="Nome do jogador" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) =>
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          }
+          horizontal
+        />
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
+      <FlatList
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) =>
+          <PlayerCard
+            name={item}
+            onRemove={() => {
+            }}
+          />
+        }
+        ListEmptyComponent={
+          <ListEmpty message="Não há pessoas nesse time." />
+        }
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 }
+        ]}
+      />
+      <Button title="Remover Time" type="SECONDARY" />
     </Container>
   );
 }
