47a31b0322e96b81a40e7d8a1eaeef3bcd2474c9
thalisson-danelon <thalisson.github@gmail.com>
2024-08-18 21:33:45 -0300

    feat: removed header from routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -7,7 +7,7 @@ const { Navigator, Screen } = createNativeStackNavigator();
 
 export function AppRoutes() {
   return (
-    <Navigator>
+    <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="teams" component={Teams} />
       <Screen name="players" component={Players} />
       <Screen name="newTeam" component={NewTeam} />
